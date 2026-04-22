6aa5951869a06cbceffeb8d596e5cb4c0efbf1ba
Dinko <dinko@ballbounce.app>
2026-04-22 18:04:45 +0300

    fix: main.dart cleanup - unused import removed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flame/flame.dart';
-import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 import 'screens/game_screen.dart';
 
@@ -8,17 +7,4 @@ void main() async {
   await Flame.device.fullScreen();
   await Flame.device.setLandscape();
   runApp(GameScreen());
-}
-
-class BallBounceBlitzApp extends StatelessWidget {
-  const BallBounceBlitzApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Ball Bounce Blitz',
-      debugShowCheckedModeBanner: false,
-      home: GameScreen(),
-    );
-  }
 }
